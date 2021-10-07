baacacc100681ee39a381de57bf7651cba5d84e8
samuel kahura <kahurasamuel@gmail.com>
2021-10-07 18:10:20 -0400

    routes

--- Server/routes/workouts.js
+++ Server/routes/workouts.js
@@ -1,11 +1,20 @@
 import express from "express";
 
-import { getWorkouts, createWorkout } from "../controllers/workouts.js";
+import { getWorkouts, createWorkout, updateWorkout, deleteWorkout } from "../controllers/workouts.js";
 
 const router = express.Router();
 
 // http://localhost:3001/workouts
+
+
+// get workout
+router.delete('/:id', deleteWorkout);
+// get workout
 router.get('/', getWorkouts);
+// post workout
 router.post('/', createWorkout);
+// update workout
+router.patch('/:id', updateWorkout)
+
 
 export default router
