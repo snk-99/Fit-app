e81a5d0d20538e9b6ea7368a4f9c3808c0caa22e
samuel kahura <kahurasamuel@gmail.com>
2021-10-08 10:33:48 -0400

    refactor to use .env

---
+++ Server/config/connection.js
@@ -0,0 +1,16 @@
+// const mongoose = require('mongoose');
+// import mongoose from "mongoose";
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fit-app', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// })
+
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+
+// export default mongoose
+
+
